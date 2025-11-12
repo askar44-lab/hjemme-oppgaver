33d8dbcb2931c025ba12b448927a1ac5fabe4fd7
Chopped-out2026 <suhaybaskar44@gmail.com>
2025-11-12 08:55:23 +0100

    Add files via upload

---
+++ javaøving.js
@@ -0,0 +1,20 @@
+//Variablar og navngjeving
+//Kva er feil eller dårleg med variabelnavna under? 
+// Gje ein kort forklaring og gje dei betre variabelnavn.
+
+let etTall = 123 //Sier ikke hva tallet skal brukes til, bedre navn kan være (antall epler)
+let variabel3 = [3, 3, 3] //Navnet variabel3 sier ikke noen om inholdet i listen bedre navn kan være (tall liste)
+let variabel1 = "teksten skal hit" //Det er en dårlig navn bedre navn kan være (let tekst)
+let detFørsteTallet = "1" //Bra navn men tallet 1 er en tekst, kanskje ta vekk ""
+let prosentAvTallet = 15 //Navnet er for langt, bare prosent passer
+let summenAvRegnestykketEr = 16 //Alt for langt navn, sum passer
+let 2tall = 5 //Starter med tall, går ikke, men tall paser som navn.
+let for = 10 //For er en løkke, passer ikke bra, tall passer.
+let mitt-tall = 7 //Mitt-tall er ikke bra nok, tall passer
+
+//Her er nokre gode eksempel, forklar gjerne kvifor dei er gode:
+
+let antallElevar = 23 //Forståelig
+let elevNavn = "Ola" //Sier navnet på elevn
+let prosentFullført = 85 //Sier at prosenten er fullført
+let sum = 42 //Sier at sumen er 42
