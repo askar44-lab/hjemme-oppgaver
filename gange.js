586e078b05090a75de4287756ac63fbb209f170d
Chopped-out2026 <suhaybaskar44@gmail.com>
2025-11-12 09:06:40 +0100

    Add files via upload

---
+++ gange.js
@@ -0,0 +1,7 @@
+//3. Variablar og bruken av disse
+//Skriv eit program som skriver "5 ganger 10 er 50" til konsoll ved å bruke variablar for tala 5, 10 og 50.
+
+let tall1 = 5;
+let tall2 = 10;
+let sum = tall1 * tall2
+console.log(tall1 + " ganger " + tall2 + " er " + sum);
