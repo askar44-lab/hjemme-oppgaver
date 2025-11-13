42a387fe405fc00ba455654d9e95b1c9cc0e3b49
Chopped-out2026 <suhaybaskar44@gmail.com>
2025-11-13 12:46:10 +0100

    Add files via upload

---
+++ variabler6.js
@@ -0,0 +1,14 @@
+//6. Variabler,
+//Hva er feil med koden i "Kodebit 1" under? Rett den opp så den kjører.
+//Skriv adressen ut på følgende to måter ved å bruke variabler: 
+//Adressen er Kongens gate 432b og Gaten er Kongens Gate, husnummeret er 432, oppgang b.
+
+
+//fikset koden
+let gate = "Kongens gate"
+let husnr = 432
+let oppgang = "b"
+let adresse = gate + husnr + oppgang
+let vei = ("Gaten er " + gate + ", husnummeret er" + ", oppgang"+ oppgang)
+console.log(vei)
+
